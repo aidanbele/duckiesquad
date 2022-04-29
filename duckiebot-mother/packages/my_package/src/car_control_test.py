9bf644cf76716d6b99ea416474a0b979b929a224
mcartervt <chipper555@protonmail.com>
2022-04-29 17:13:56 -0400

    wrote a for loop to make the robot go in a square

--- duckiebot-mother/packages/my_package/src/car_control_test.py
+++ duckiebot-mother/packages/my_package/src/car_control_test.py
@@ -5,15 +5,26 @@ import rospy
 from duckietown_msgs.msg import CarControl
 
 if __name__ == '__main__':
-    # List of tuples in the form (speed, steering, sleep_time)
-    command_list = [(1.0, 0.0, 1)]
     # Initialize the node with rospy
     rospy.init_node('dagu_car_tester', anonymous=False)
     pub = rospy.Publisher("~car_control",CarControl,queue_size=1)
     rospy.loginfo("[dagu_car_tester] Initializing.")
     rospy.sleep(1.0)
+
+    # List of tuples in the form (speed, steering, sleep_time)
+    command_list = [(1.0, 0.0, 1), (1.0, 1.0, 3)]
+    # Loop 4 times to go in a square
+    for i in range(4):
+        for j in range(len(command_list)):
+            pub.publish(CarControl(speed=command_list[j][0],steering=command_list[j][1]))
+            if j == 0:
+                rospy.loginfo("[dagu_car_tester] Full speed ahead.")
+            else:
+                rospy.loginfo("[dagu_car_tester] Full speed left turn.")
+            rospy.sleep(command_list[j][2])
+    
     # Full speed ahead
-    pub.publish(CarControl(speed=1.0,steering=0.0))
+    """ pub.publish(CarControl(speed=1.0,steering=0.0))
     rospy.loginfo("[dagu_car_tester] Full speed ahead.")
     rospy.sleep(3.0)
     # Full speed reverse
@@ -27,7 +38,7 @@ if __name__ == '__main__':
     # Full speed left turn
     pub.publish(CarControl(speed=1.0,steering=-1.0))
     rospy.loginfo("[dagu_car_tester] Full speed right turn")
-    rospy.sleep(3.0)
+    rospy.sleep(3.0) """
     # Stop
     pub.publish(CarControl(speed=0.0,steering=0.0))
     rospy.loginfo("[dagu_car_tester] Stop")
